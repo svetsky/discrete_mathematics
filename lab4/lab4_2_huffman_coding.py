eb03ecb603c1df704e9474d59c49dfb1c522d90d
svetsky <166009586+svetsky@users.noreply.github.com>
2025-03-20 17:45:33 +0200

    добавлен бинарный код для букв

--- lab4/lab4_2_huffman_coding.py
+++ lab4/lab4_2_huffman_coding.py
@@ -4,8 +4,9 @@
 описание алгоритма Хаффмана (страница 459) https://matematika76.ru/fm/Кормен.pdf
 '''
 from queue import PriorityQueue
+import itertools
 
-# дерево
+# Дерево
 class Node:
     def __init__(self, symbol=None, frequency=None):
         self.symbol = symbol
@@ -13,31 +14,59 @@ class Node:
         self.left = None
         self.right = None
 
+    def __lt__(self, other):
+        return self.frequency < other.frequency
+
+'''
 # создание C - словарь, в котором ключи - буквы, а значения - частоты соответствующих букв
 C = {}
 file = open('letter_frequencies.txt')
 for line in file:
-    C[line[0]] = int(line[3:-1])
+    char, frequency = line.split()
+    C[char] = int(frequency)
 file.close()
+'''
+C = {'A': 10, 'B': 5, 'C': 7, 'D': 1, 'E': 6}
 n = len(C)  # количество различных символов в тексте
 
-# добавляем все элементы словаря C в очередь с приоритетом Q
+# Добавляем все элементы словаря C в очередь с приоритетом Q
 Q = PriorityQueue()
-for key in C:
-    char = Node(key, C[key])
-    Q.put((C[key], char))
+unique_id = itertools.count() 
+for key, freq in C.items():
+    char = Node(key, freq)
+    Q.put((freq, next(unique_id), char))  
 
-# алгоритм Хаффмана (строим дерево)
-for i in range(n-1):
-    z = Node()
-    char1 = Q.get()
-    char2 = Q.get()
-    z.left = char1[1]
-    z.right = char2[1]
-    z.frequency = char1[1].frequency + char2[1].frequency
-    Q.put((z.frequency, z))
+# Алгоритм Хаффмана (строим дерево)
+for i in range(n - 1):
+    # Берём два узла с наименьшей частотой
+    freq1, _, char1 = Q.get()
+    freq2, _, char2 = Q.get()
 
-root = Q.get()
+    # Создаём новый узел z
+    z = Node(frequency=freq1 + freq2)
+    z.left = char1
+    z.right = char2
+    Q.put((z.frequency, next(unique_id), z)) 
+
+# корень дерева
+root = Q.get()[2]
+
+# Функция для генерации кодов Хаффмана
+def generate_huffman_codes(node, code="", code_dict=None):
+    if code_dict is None:
+        code_dict = {}
+    if node.symbol:
+        code_dict[node.symbol] = code
+    else:
+        generate_huffman_codes(node.left, code + "0", code_dict)
+        generate_huffman_codes(node.right, code + "1", code_dict)
+    return code_dict
+
+
+huffman_codes = generate_huffman_codes(root)
+print("Коды Хаффмана:")
+for symbol, code in huffman_codes.items():
+    print(f"Символ: {symbol}, Код: {code}")
 
 
 
